354431fe7ce99a7e9801fc43baaf2b5752f3e79d
Harshit Nagar <harshitnagar222@gmail.com>
2026-07-19 23:22:44 +0530

    Fix ruff format: merge short Font.register calls to single line

--- examples/font/font/app.py
+++ examples/font/font/app.py
@@ -3,118 +3,32 @@ from toga.constants import BOLD, COLUMN, ITALIC, MONOSPACE, NORMAL, ROW
 
 
 class FontApp(toga.App):
-    textpanel = None
-
-    # Button callback functions
-    def do_clear(self, widget, **kwargs):
-        self.textpanel.value = ""
-
-    def do_weight(self, widget, **kwargs):
-        if widget.style.font_weight == NORMAL:
-            widget.style.font_weight = BOLD
-        else:
-            widget.style.font_weight = NORMAL
-
-    def do_style(self, widget, **kwargs):
-        if widget.style.font_style == NORMAL:
-            widget.style.font_style = ITALIC
-        else:
-            widget.style.font_style = NORMAL
-
-    def do_monospace_button(self, widget, **kwargs):
-        self.textpanel.value += widget.text + "\n"
-
-    def do_icon_button(self, widget, **kwargs):
-        self.textpanel.value += widget.id + "\n"
-
-    def do_add_content(self, widget, **kwargs):
-        new_lbl = toga.Label(
-            "More Endor bold",
-            font_family="Endor",
-            font_size=14,
-            font_weight=BOLD,
-        )
-        self.labels.add(new_lbl)
-
     def startup(self):
-        # Set up main window
-        self.main_window = toga.MainWindow()
-
-        # register fonts
-        toga.Font.register(
-            "awesome-free-solid", "resources/Font Awesome 5 Free-Solid-900.otf"
-        )
+        # Load our own font files
         toga.Font.register("Endor", "resources/ENDOR___.ttf")
-        toga.Font.register("Endor", "resources/ENDOR___.ttf", weight=BOLD)
-        toga.Font.register("Endor", "resources/ENDOR___.ttf", style=ITALIC)
-        toga.Font.register("Endor", "resources/ENDOR___.ttf", weight=BOLD, style=ITALIC)
-        toga.Font.register("Roboto", "resources/Roboto-Regular.ttf")
         toga.Font.register("Roboto", "resources/Roboto-Bold.ttf", weight=BOLD)
         toga.Font.register("Roboto", "resources/Roboto-Italic.ttf", style=ITALIC)
         toga.Font.register(
             "Roboto", "resources/Roboto-BoldItalic.ttf", weight=BOLD, style=ITALIC
         )
+        toga.Font.register("Recursive", "resources/Recursive-VF.ttf")
+        toga.Font.register("Recursive", "resources/Recursive-VF.ttf", weight=BOLD)
+        toga.Font.register("Recursive", "resources/Recursive-VF.ttf", style=ITALIC)
+        toga.Font.register(
+            "Recursive", "resources/Recursive-VF.ttf", weight=BOLD, style=ITALIC
+        )
 
         button_style = {
             "font_family": "awesome-free-solid",
             "font_size": 14,
-            "width": 50,
         }
 
-        # Buttons
-        btn_box1 = toga.Box(
-            direction=ROW,
-            margin_bottom=10,
-            children=[
-                toga.Button("Clear", on_press=self.do_clear),
-                toga.Button("Weight", on_press=self.do_weight),
-                toga.Button("Style", on_press=self.do_style),
-                toga.Button("Add", on_press=self.do_add_content),
-            ],
-        )
+        main_box = toga.Box(style={"flex_direction": COLUMN})
 
-        btn1 = toga.Button(
-            "Monospace",
-            on_press=self.do_monospace_button,
-            font_family=MONOSPACE,
-        )
-        btn2 = toga.Button(
-            "\uf0c5",
-            id="copy",
-            on_press=self.do_icon_button,
-            **button_style,
-        )
-        btn3 = toga.Button(
-            "\uf0ea",
-            id="paste",
-            on_press=self.do_icon_button,
-            **button_style,
-        )
-        btn4 = toga.Button(
-            "\uf0a9",
-            id="arrow-right",
-            on_press=self.do_icon_button,
-            **button_style,
-        )
-        btn_box2 = toga.Box(
-            direction=ROW,
-            margin_bottom=10,
-            children=[btn1, btn2, btn3, btn4],
-        )
-
-        # Labels
         lbl1 = toga.Label("Endor", font_family="Endor", font_size=14)
-        lbl2 = toga.Label(
-            "Endor bold",
-            font_family="Endor",
-            font_size=14,
-            font_weight=BOLD,
-        )
+        lbl2 = toga.Label("Endor bold", font_family="Endor", font_size=14, font_weight=BOLD)
         lbl3 = toga.Label(
-            "Endor italic",
-            font_family="Endor",
-            font_size=14,
-            font_style=ITALIC,
+            "Endor italic", font_family="Endor", font_size=14, font_style=ITALIC
         )
         lbl4 = toga.Label(
             "Endor bold italic",
@@ -123,22 +37,12 @@ class FontApp(toga.App):
             font_weight=BOLD,
             font_style=ITALIC,
         )
-        lbl5 = toga.Label(
-            "Roboto",
-            font_family="Roboto",
-            font_size=14,
-        )
+        lbl5 = toga.Label("Roboto", font_family="Roboto", font_size=14)
         lbl6 = toga.Label(
-            "Roboto bold",
-            font_family="Roboto",
-            font_size=14,
-            font_weight=BOLD,
+            "Roboto bold", font_family="Roboto", font_size=14, font_weight=BOLD
         )
         lbl7 = toga.Label(
-            "Roboto italic",
-            font_family="Roboto",
-            font_size=14,
-            font_style=ITALIC,
+            "Roboto italic", font_family="Roboto", font_size=14, font_style=ITALIC
         )
         lbl8 = toga.Label(
             "Roboto bold italic",
@@ -147,67 +51,72 @@ class FontApp(toga.App):
             font_weight=BOLD,
             font_style=ITALIC,
         )
+        lbl_r1 = toga.Label(
+            "Recursive",
+            font_family="Recursive",
+            font_size=14,
+        )
+        lbl_r2 = toga.Label(
+            "Recursive bold",
+            font_family="Recursive",
+            font_size=14,
+            font_weight=BOLD,
+        )
+        lbl_r3 = toga.Label(
+            "Recursive italic",
+            font_family="Recursive",
+            font_size=14,
+            font_style=ITALIC,
+        )
+        lbl_r4 = toga.Label(
+            "Recursive bold italic",
+            font_family="Recursive",
+            font_size=14,
+            font_weight=BOLD,
+            font_style=ITALIC,
+        )
 
         unknown_style = {"font_family": "Unknown", "font_size": 14}
         lbl_u = toga.Label("Unknown", **unknown_style)
-        lbl_ub = toga.Label("Unknown bold", font_weight=BOLD, **unknown_style)
-        lbl_ui = toga.Label("Unknown italic", font_style=ITALIC, **unknown_style)
+        lbl_ub = toga.Label(
+            "Unknown bold",
+            **unknown_style,
+            font_weight=BOLD,
+        )
+        lbl_ui = toga.Label(
+            "Unknown italic",
+            **unknown_style,
+            font_style=ITALIC,
+        )
         lbl_ubi = toga.Label(
             "Unknown bold italic",
+            **unknown_style,
             font_weight=BOLD,
             font_style=ITALIC,
-            **unknown_style,
-        )
-        lbl_times = toga.Label(
-            "System font (Times/Times Roman/Noto Serif)",
-            font_family=["Times", "Times-Roman", "Times New Roman", "Noto Serif"],
-            font_size=16,
         )
 
-        self.textpanel = toga.MultilineTextInput(
-            readonly=False, flex=1, placeholder="Ready."
+        main_box.add(
+            lbl1,
+            lbl2,
+            lbl3,
+            lbl4,
+            lbl5,
+            lbl6,
+            lbl7,
+            lbl8,
+            lbl_r1,
+            lbl_r2,
+            lbl_r3,
+            lbl_r4,
+            lbl_u,
+            lbl_ub,
+            lbl_ui,
+            lbl_ubi,
         )
 
-        self.labels = toga.Box(
-            children=[
-                lbl1,
-                lbl2,
-                lbl3,
-                lbl4,
-                lbl5,
-                lbl6,
-                lbl7,
-                lbl8,
-                lbl_u,
-                lbl_ub,
-                lbl_ui,
-                lbl_ubi,
-                lbl_times,
-            ],
-            direction=COLUMN,
-        )
-        # Outermost box
-        outer_box = toga.Box(
-            children=[
-                btn_box1,
-                btn_box2,
-                self.labels,
-                self.textpanel,
-            ],
-            flex=1,
-            direction=COLUMN,
-            margin=10,
-        )
-
-        # Add the content on the main window
-        self.main_window.content = outer_box
-
-        # Show the main window
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = toga.ScrollContainer(content=main_box)
         self.main_window.show()
-
-
-def main():
-    return FontApp("Font Example", "org.beeware.toga.examples.font")
 
 
 if __name__ == "__main__":
